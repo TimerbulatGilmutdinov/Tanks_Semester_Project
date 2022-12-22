990eea3d9763f4dc32b10933f199a75aa4cd00df
Timerbulat <TimerbulatGilmutdinov@github.com>
2022-12-22 18:43:48 +0300

    added constants package, first steps in implementing GET_ALL method

---
+++ Tanks-protocol/src/main/java/constants/HeaderName.java
@@ -0,0 +1,9 @@
+package constants;
+
+public enum HeaderName {
+    TANK_COORD_X,
+    TANK_COORD_Y,
+    WEAPON_ANGLE,
+    X_BULLET_DIRECTION,
+    Y_BULLET_DIRECTION
+}
---
+++ Tanks-protocol/src/main/java/constants/Protocol.java
@@ -0,0 +1,6 @@
+package constants;
+
+public class Protocol {
+    public static final String NAME = "tnkp";
+    public static final String VERSION = "1.0";
+}
