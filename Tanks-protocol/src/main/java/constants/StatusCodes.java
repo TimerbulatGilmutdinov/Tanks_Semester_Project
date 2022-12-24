a1ae2665ced2f2d3f67675ffbb5d95cf5d47c52f
Timerbulat <TimerbulatGilmutdinov@github.com>
2022-12-24 18:35:49 +0300

    added status codes as const

---
+++ Tanks-protocol/src/main/java/constants/StatusCodes.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class StatusCodes {
+    public static final int OK = 200;
+    public static final int PLAYER_DISCONNECTED = 600;
+    public static final int INVALID_REQUEST = 900;
+}
