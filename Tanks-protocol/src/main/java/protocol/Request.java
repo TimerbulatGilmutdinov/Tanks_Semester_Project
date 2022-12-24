b864fc83b0310d673d3c7329e55be48f69354816
Timerbulat <TimerbulatGilmutdinov@github.com>
2022-12-24 18:29:24 +0300

    changed constructor of request&response

--- Tanks-protocol/src/main/java/protocol/Request.java
+++ Tanks-protocol/src/main/java/protocol/Request.java
@@ -3,16 +3,15 @@ package protocol;
 import constants.MethodName;
 import lombok.Getter;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @Getter
 public class Request {
-    private MethodName methodName;
-    private final Message message;
-    private Map<String,String> headersMap = new HashMap<>();
+    private final MethodName methodName;
+    private final Map<String,String> headersMap;
 
-    public Request(Message message){
-        this.message = message;
+    public Request(MethodName methodName, Map<String,String> headersMap){
+        this.methodName = methodName;
+        this.headersMap = headersMap;
     }
 }
--- Tanks-protocol/src/main/java/protocol/Response.java
+++ Tanks-protocol/src/main/java/protocol/Response.java
@@ -1,15 +1,22 @@
 package protocol;
 
 import constants.MethodName;
+import lombok.Getter;
 
-import java.util.HashMap;
 import java.util.Map;
 
+@Getter
 public class Response {
-    private MethodName methodName;
-    private final Message message;
+    private int statusCode;
+    private final MethodName methodName;
+    private final Map<String,String> headersMap;
 
-    public Response(Message message) {
-        this.message = message;
+    public Response(MethodName methodName,Map<String,String> headersMap) {
+        this.methodName = methodName;
+        this.headersMap = headersMap;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
     }
 }
