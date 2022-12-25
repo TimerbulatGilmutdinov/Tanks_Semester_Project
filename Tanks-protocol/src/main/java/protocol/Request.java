31d6a516609e369afcd6158afdaf055f48dd0fbc
Timerbulat <TimerbulatGilmutdinov@github.com>
2022-12-25 19:26:28 +0300

    added version to Request constructor

--- Tanks-protocol/src/main/java/protocol/Request.java
+++ Tanks-protocol/src/main/java/protocol/Request.java
@@ -2,19 +2,24 @@ package protocol;
 
 import constants.Entity;
 import constants.MethodName;
+import constants.ProtocolInfo;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.util.Map;
 
+@Builder
 @Getter
 public class Request {
     private final MethodName methodName;
     private final Entity entity;
+    private final String version;
     private final Map<String,String> headersMap;
 
-    public Request(MethodName methodName, Entity entity, Map<String,String> headersMap){
+    public Request(MethodName methodName, Entity entity, String version, Map<String,String> headersMap){
         this.methodName = methodName;
         this.entity = entity;
+        this.version = version;
         this.headersMap = headersMap;
     }
 }
