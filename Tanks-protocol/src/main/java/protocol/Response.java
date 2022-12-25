7dc72f954abd8b905c4366ca6e61de5b841f2ef2
Timerbulat <TimerbulatGilmutdinov@github.com>
2022-12-25 22:14:31 +0300

    some changes to Response class

--- Tanks-protocol/src/main/java/protocol/Response.java
+++ Tanks-protocol/src/main/java/protocol/Response.java
@@ -2,20 +2,25 @@ package protocol;
 
 import constants.Entity;
 import constants.MethodName;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.util.Map;
 
+@Builder
 @Getter
 public class Response {
-    private int statusCode;
     private final MethodName methodName;
     private final Entity entity;
+    private final String version;
+    private int statusCode;
     private final Map<String,String> headersMap;
 
-    public Response(MethodName methodName,Entity entity,Map<String,String> headersMap) {
+    public Response(MethodName methodName,Entity entity, String version, int statusCode,Map<String,String> headersMap) {
         this.methodName = methodName;
         this.entity = entity;
+        this.version = version;
+        this.statusCode = statusCode;
         this.headersMap = headersMap;
     }
 
