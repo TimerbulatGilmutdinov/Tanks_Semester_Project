2e2304deb2142750a59fd65bc1ecfa3a360e3026
aidar <ajdar.gafiat@gmail.com>
2022-12-27 17:04:18 +0300

    added client

--- Tanks-client/net/src/main/java/ru/kpfu/itis/RequestBuilder.java
+++ Tanks-client/net/src/main/java/ru/kpfu/itis/RequestBuilder.java
@@ -10,4 +10,5 @@ public class RequestBuilder {
 //    public static Request build(MethodName methodName, Entity entity, Map<String, String> headers) {
 //        return new Request(methodName, entity, "tnkp/1.0", headers);
 //    }
+
 }
